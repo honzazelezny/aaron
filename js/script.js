d7d0ede6918da9b2cd5e2f0d55d230cf0e9ad788
honzazelezny <76755298+honzazelezny@users.noreply.github.com>
2023-07-06 22:25:59 +0200

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,57 @@
+let wow = new WOW(
+  {
+    boxClass:     'wow',      // animated element css class (default is wow)
+    animateClass: 'animated', // animation css class (default is animated)
+    offset:       0,          // distance to the element when triggering the animation (default is 0)
+    mobile:       true,       // trigger animations on mobile devices (default is true)
+    live:         true,       // act on asynchronously loaded content (default is true)
+    callback:     function(box) {
+      // the callback is fired every time an animation is started
+      // the argument that is passed in is the DOM node being animated
+    },
+    scrollContainer: null // optional scroll container selector, otherwise use window
+  }
+);
+wow.init();
+//  галерея
+$(".gallery_list").magnificPopup({
+    delegate: "a",
+    type: "image",
+    gallery: {
+      enabled: true
+    }
+  });
+  // гамбургер
+  window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+});
+// плавный скролл
+var $page = $('html, body');
+$('a[href*="#"]').click(function() {
+    $page.animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 300);
+    return false;
+});
+//preloader
+// функция для скрытия прелоадера после 3 секунд
+function hidePreloader() {
+  document.getElementById("preloader").style.display = "none";
+}
+
+// задержка прелоадера в 3 секунды
+setTimeout(hidePreloader, 2500);
